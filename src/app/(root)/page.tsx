f2b527d3e570e34751017e741f0b0541fc20e798
Cody Milliron <codymilliron.dev@gmail.com>
2025-01-07 00:09:52 -0500

    add loading page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,6 @@
-export default function Homepage() {
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function Homepage() {
+  await delay(2000);
   return <h1>HomePage</h1>;
 }
