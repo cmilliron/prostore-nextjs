3841af3b878b947c11d27a177faf9498eed7f18f
Cody Milliron <codymilliron.dev@gmail.com>
2025-01-23 21:58:30 -0500

    Add cart details to grid

--- src/app/(root)/cart/cart-table.tsx
+++ src/app/(root)/cart/cart-table.tsx
@@ -8,10 +8,20 @@ import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function CartTable({ cart }: { cart?: Cart }) {
   const router = useRouter();
-  const toast = useToast();
+  const { toast } = useToast();
   const [isPending, startTransition] = useTransition();
   return (
     <>
@@ -22,7 +32,81 @@ export default function CartTable({ cart }: { cart?: Cart }) {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3"></div>
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        ></Image>
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={async () => {
+                          const res = await removeItemFromCart(item.productId);
+                          if (!res.success) {
+                            toast({
+                              variant: "destructive",
+                              description: res.message,
+                            });
+                          }
+                        }}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={async () => {
+                          const res = await addItemToCart(item);
+                          if (!res.success) {
+                            toast({
+                              variant: "destructive",
+                              description: res.message,
+                            });
+                          }
+                        }}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
