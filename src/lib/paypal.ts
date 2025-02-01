38bb99a1ebac2266eba16490300a9c13fbb60957
Cody Milliron <codymilliron.dev@gmail.com>
2025-02-01 17:02:45 -0500

    Add createOrder and capturePayment from PayPal

--- src/lib/paypal.ts
+++ src/lib/paypal.ts
@@ -1,7 +1,47 @@
 const baseUrl =
   process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${baseUrl}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authoriazation: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${baseUrl}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authoriazation: `Bearer ${accessToken}`,
+      },
+    });
+
+    return handleResponse(response);
+  },
+};
 
 // Generate a access token for the PayPal API
 export async function generateAccessToken() {
