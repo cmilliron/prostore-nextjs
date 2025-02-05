f2b3d7c4f56058c5263a94b7beb02d24eaf62f6e
Cody Milliron <codymilliron.dev@gmail.com>
2025-02-04 23:15:23 -0500

    Add order listing page in admin panel

---
+++ src/app/(admin)/admin/orders/page.tsx
@@ -0,0 +1,94 @@
+import { getCurrentSession } from "@/lib/actions/auth-actions";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import Pagination from "@/components/shared/pagination";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Admin Orders",
+};
+
+export default async function OrdersPage(props: {
+  searchParams: Promise<{ page: string }>;
+}) {
+  const { page = "1" } = await props.searchParams;
+
+  const session = await getCurrentSession();
+  if (session?.user.role !== "admin")
+    throw new Error("Admin permission required");
+
+  const orders = await getAllOrders({ page: Number(page) });
+
+  console.log(orders);
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>
+                  {formatCurrency(order.totalPrice.toNumber())}
+                </TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : "not paid"}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "not delivered"}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>
+                      <span className="px-2">Details</span>
+                    </Link>
+                  </Button>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>
+                      <span className="px-2">Delete</span>
+                    </Link>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
