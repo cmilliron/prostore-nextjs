60d9516cd85bfb2ada60f48df61033446c70e94d
Cody Milliron <codymilliron.dev@gmail.com>
2025-02-18 21:33:15 -0500

    Add abiility to pull products into admin page

--- src/app/(admin)/admin/products/page.tsx
+++ src/app/(admin)/admin/products/page.tsx
@@ -1,15 +1,34 @@
+import Link from "next/link";
+import { getAllProducts } from "@/lib/actions/products-actions";
+import { formatCurrency, formatId } from "@/lib/utils";
+
+interface AdminProductSearchParams {
+  page?: string;
+  query?: string;
+  category?: string;
+}
+
 export default async function AdminProductsPage(props: {
-  searchPrarms: Promise<{
+  searchParams: Promise<{
     page: string;
     query: string;
     category: string;
   }>;
 }) {
-  const searchParams = await props.searchPrarms;
+  const searchParams = await props.searchParams;
 
-  const page = Number(searchParams.page) || 1;
-  const searchText = searchParams.query || "";
-  const category = searchParams.category || "";
+  const page = Number(searchParams?.page) || 1;
+
+  const searchText = searchParams?.query || "";
+  const category = searchParams?.category || "";
+
+  const products = await getAllProducts({
+    query: searchText,
+    page,
+    category,
+  });
+
+  console.log(products);
 
   return (
     <div className="space-y-2">
