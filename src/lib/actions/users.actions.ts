e7d1a82c7d34ec4508af9da09204efaa4b9f67e8
Cody Milliron <codymilliron.dev@gmail.com>
2025-02-27 19:14:12 -0500

    add updateUser to user.action file

--- src/lib/actions/users.actions.ts
+++ src/lib/actions/users.actions.ts
@@ -7,6 +7,7 @@ import {
   signInFormSchema,
   signUpFormSchema,
   paymentMethodSchema,
+  updateUserSchema,
 } from "../validators";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
@@ -214,6 +215,28 @@ export async function deleteUserById(id: string) {
     return {
       success: true,
       message: "user deleted successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update User
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath("/admin/users");
+
+    return {
+      success: true,
+      message: "User updated successfully",
     };
   } catch (error) {
     return { success: false, message: formatError(error) };
