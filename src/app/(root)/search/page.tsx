42589ca226e6e4c30a8b50c9f48ddf0270555e59
Cody Milliron <codymilliron.dev@gmail.com>
2025-03-03 17:24:28 -0500

    Add filters

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -28,6 +28,9 @@ export default async function SearchPage(props: {
 
   console.log(q, category, price, rating, sort, page);
 
+  // Get Categories
+  const categories = await getAllCategories();
+
   //   Get Products
   const products = await getAllProducts({
     category,
@@ -37,11 +40,62 @@ export default async function SearchPage(props: {
     page: Number(page),
     sort,
   });
+
+  // Construct filter url
+  const getFilterUrl = ({
+    c,
+    s,
+    p,
+    r,
+    pg,
+  }: {
+    c?: string;
+    s?: string;
+    p?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+    if (s) params.sort = s;
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* Filters */}</div>
+      <div className="filter-links">
+        {/* Filters */}
+        <div className="text-xl mt-3 mb-2">Department</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${
+                  ("all" === category || "" === category) && "font-bold"
+                }`}
+                href={getFilterUrl({ c: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {categories.map((x) => (
+              <li key={x.category}>
+                <Link
+                  className={`${x.category === category && "font-bold"}`}
+                  href={getFilterUrl({ c: x.category })}
+                >
+                  {x.category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div className="md:col-span-4 space-y-4">
-        <div className="grid grid-cols-1 gpa-4 md:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products!.data.length === 0 && <div>No product found</div>}
           {products!.data.map((product) => (
             <ProductCard key={product.id} product={product} />
