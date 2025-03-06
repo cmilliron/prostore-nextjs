d4a90fcc75c97495708f53a69fe4e14d73427ee4
Cody Milliron <codymilliron.dev@gmail.com>
2025-03-05 19:48:13 -0500

    Add metadata to search page

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -8,6 +8,41 @@ import {
 import Link from "next/link";
 import { prices, ratings, sortOrders } from "@/lib/constants/filters";
 
+export async function generateMetadata(props: {
+  searchParams: Promise<{
+    q: string;
+    category: string;
+    price: string;
+    rating: string;
+  }>;
+}) {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `Search ${isQuerySet ? q : ""}
+      ${isCategorySet ? `: Category ${category}` : ""}
+      ${isPriceSet ? `: Price ${price}` : ""}
+      ${isRatingSet ? `: Rating ${rating}` : ""}`,
+    };
+  } else {
+    return {
+      title: `Search Products`,
+    };
+  }
+}
+
 export default async function SearchPage(props: {
   searchParams: Promise<{
     q?: string;
