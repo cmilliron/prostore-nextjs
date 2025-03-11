ee8b063634234c3896ca791d9d3d65e14b904d52
Cody Milliron <codymilliron.dev@gmail.com>
2025-03-10 21:34:27 -0400

    Try aother things to fix vercel deployment 2

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,10 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
+export const config = {
+  runtime: "nodejs",
+};
+
 //  Export  routes for next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
